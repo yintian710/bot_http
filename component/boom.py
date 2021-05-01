23c073b20be1b9e43fc8afc5f0a989c3dbca4535
like-a-loach <z_yizhong@sina.com>
2021-05-01 16:25:19 +0800

    new boom

---
+++ component/boom.py
@@ -0,0 +1,22 @@
+
+boom = 0
+
+
+def new_boom():
+    """
+    新建一个boom数,并将它设为全局变量
+    但是,在boom不为0的时候,要拒绝这个请求
+    :return:
+    """
+    global boom
+    pass
+
+
+def boom_play(user_id, boom_num):
+    """
+    判断boom_num与boom的大小,并提示接下来的游戏范围,当一个人猜对时,归零boom
+    :param user_id:
+    :param boom_num:
+    :return:
+    """
+    pass
