2b4c37de4aa25de7467ce7665c6ea3f43ddb380f
yintian710 <yintian710@gmail.com>
2021-06-12 00:50:08 +0800

    吃啥 后台md

---
+++ component/eat.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+@File  : eat.py
+@Author: yintian
+@Date  : 2021/6/11 23:43
+@Desc  : 
+"""
+if __name__ == '__main__':
+    pass
--- component/score.py
+++ component/score.py
@@ -22,30 +22,33 @@ def daily_score(user_id):
     """
     score, da = select_u(user_id, 'score', 'da')
     today = str(datetime.date.today())
-    # if da == today:
-    #     return get_return('臭不要脸签两次的爬！', 1)
+    if da == today:
+        return get_return('您已签到！', 1)
     a = random.randint(20, 50)
-    if a <= 24:
-        str2 = '不会真有人觉得自己签个到就能欧皇吧？不会吧不会吧？\n非酋签个到也才'
-    elif a >= 41:
-        str2 = '欧皇ohhh!!\n签到居然有'
-    else:
-        str2 = '...\n获得:'
-    # if score >= 500:
-    #     a = int(a*0.1)
-    #     str1 = '\n积分超过五百，签到积分减少90%，'
-    # elif score >= 400:
-    #     a = int(a*0.6)
-    #     str1 = '\n积分超过四百，签到积分减少60%,'
-    # elif score >= 300:
-    #     a = int(a*0.5)
-    #     str1 = '\n积分超过三百，签到积分减少50%,'
-    # elif score >= 200:
-    #     a = int(a*0.6)
-    #     str1 = '\n积分超过二百，签到积分减少40%,'
     score += a
     change_score(user_id, score, today)
-    return get_return(str2 + f'{a}积分！总积分:{score}')
+    str1 = f'获得:{a}积分！总积分:{score}'
+    # if a <= 24:
+    #     str2 = '不会真有人觉得自己签个到就能欧皇吧？不会吧不会吧？\n非酋签个到也才'
+    # elif a >= 41:
+    #     str2 = '欧皇ohhh!!\n签到居然有'
+    # else:
+    #     str2 = '...\n获得:'
+    # # if score >= 500:
+    # #     a = int(a*0.1)
+    # #     str1 = '\n积分超过五百，签到积分减少90%，'
+    # # elif score >= 400:
+    # #     a = int(a*0.6)
+    # #     str1 = '\n积分超过四百，签到积分减少60%,'
+    # # elif score >= 300:
+    # #     a = int(a*0.5)
+    # #     str1 = '\n积分超过三百，签到积分减少50%,'
+    # # elif score >= 200:
+    # #     a = int(a*0.6)
+    # #     str1 = '\n积分超过二百，签到积分减少40%,'
+    #
+    # return get_return(str2 + f'')
+    return get_return(str1)
 
 
 @is_regis
