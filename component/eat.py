4daccbaf0ff1a72a25b0b7b7129697d53de56c05
yintian710 <yintian710@gmail.com>
2021-06-13 00:46:59 +0800

    eat 修改

--- component/eat.py
+++ component/eat.py
@@ -6,6 +6,7 @@
 @Desc  : 
 """
 import json
+import random
 import time
 
 from tool.common import is_regis, get_return
@@ -68,21 +69,59 @@ def get_one_restaurant(restaurants_data, restaurant_id):
 
 
 def get_restaurant(user_id):
+    """
+    获取用户当前选定食府数据
+    :param user_id:
+    :return:
+    """
     restaurant_number = get_restaurant_id(user_id)
     restaurant = get_one_restaurant(user_id, restaurant_number)
     return restaurant
 
 
 def get_restaurant_id(user_id):
+    """
+    获取当前用户选定食府id
+    :param user_id:
+    :return:
+    """
     restaurants_data = get_restaurants_data(user_id)
     restaurant_id = restaurants_data.get("0")
     return restaurant_id
 
 
 def choose_restaurant(user_id, restaurants_data, restaurant_id):
+    """
+    改变用户当前选定食府id
+    :param user_id:
+    :param restaurants_data:
+    :param restaurant_id:
+    :return:
+    """
     restaurants_data['0'] = restaurant_id
     save_restaurants_data(user_id, restaurants_data)
 
 
+def get_random_food(food: dict, ignore=None):
+    """
+    获取随即产生的食物
+    :param food: 食府中的食物数据
+    :param ignore: 需要被忽略的一个食物，往往是已经去了两次的
+    :return:
+    """
+    if ignore:
+        food.pop(ignore)
+    foods = food.keys()
+    weights = food.values()
+    random_food = random.choices(foods, weights=weights)
+    return random_food
+
+
+def go_restaurant(user_id, restaurant_id, restaurants_data):
+    food = restaurants_data['go']
+    restaurants_data[restaurant_id]['food'][food] += 5
+
+
 if __name__ == '__main__':
-    pass
+    res = get_restaurants_data('1327960105')
+    print(res)
